7bcee5bff178cd685932997a0a0c4faa39e0b530
Adrien Maranville <Adrien@Maranville.IO>
2018-11-07 21:11:38 -0700

    refactoring navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-import styled, {css} from 'styled-components';
+import styled from 'styled-components';
+import AppBar from "./AppBar";
 
 const AppLayout = styled.div`
   padding: 40px;
 `;
-const Logo = styled.div`
-  font-size: 1.5em;
-`;
-const ControlButton = styled.div`
-  cursor: pointer;
-  ${props => props.active && css`
-    text-shadow: 0px 0px 60px #03ff03;
-  `}
-`;
-const Bar = styled.div`
-  margin-bottom: 40px;
-  display: grid;
-  grid-template-columns: 180px auto 100px 100px;
-`;
+
 const Content = styled.div`
   
 `;
@@ -62,25 +50,7 @@ class App extends Component {
   render() {
     return (
       <AppLayout>
-        <Bar>
-          <Logo>
-            CryptoDash
-          </Logo>
-          <div>
-          </div>
-          {!this.state.firstVisit && (
-            <ControlButton onClick={() => {
-            this.setState({page: 'dashboard'})
-          }} active={this.displayingDashboard()}>
-            Dashboard
-          </ControlButton> )}
-          {!this.state.firstVisit && (
-          <ControlButton onClick={() => {
-            this.setState({page: 'settings'})
-          }} active={this.displayingSettings()}>
-            Settings
-          </ControlButton> )}
-        </Bar>
+        {AppBar.call(this)}
         <Content>
           {this.displayingSettings() && this.settingsContent()}
         </Content>
